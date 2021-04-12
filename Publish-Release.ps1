93547b24de40a91f29db1e28e86a98b1b86401c7
jburnett <jburnett@altamodatech.com>
2021-04-12 17:17:51 -0400

    Add NuGet pkgs publish script

---
+++ Publish-Release.ps1
@@ -0,0 +1,30 @@
+param (
+    [Parameter(Mandatory=$true)][string]$apiKey
+)
+
+$branch = invoke-expression "git branch --show-current"
+
+if ($branch -ne 'master') {
+    Write-Error "Publishing only allowed from master branch"
+    exit
+}
+
+$projPaths = @(
+    "./AMT.Extensions.Logging",
+    "./AMT.Extensions.System"
+)
+
+# Ensure everything is built
+Write-Verbose "Pack projects"
+$projPaths | %{
+    write-host ""   # separator
+    dotnet pack -c Release $_
+}
+
+# Publish to NuGet
+Write-Verbose "Publish projects"
+$projPaths | %{
+    write-host ""   # separator
+    $pkg = gci $_/bin/Release *.nupkg
+    dotnet nuget push -k $apiKey $pkg.FullName -s https://api.nuget.org/v3/index.json
+}
