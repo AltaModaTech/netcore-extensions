8d389e5135e7995daf86b9dc7c2ee9bcf4717d2d
jburnett <j.burnett@intellinet.com>
2024-05-20 00:40:28 -0400

    Include Linq extensions from AMT.LinqExtensions

---
+++ AMT.Extensions.Linq/LinqExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace AMT.Extensions.Linq
+{
+	
+	///<summary>Randomized operations for LINQ.</summary>
+	public static class LinqRandomizer
+	{
+		private static System.Random _randomizer = new System.Random(DateTime.Now.Millisecond);
+
+
+		#region Public LINQ methods
+
+		///<summary>Returns a random element from a collection.</summary>
+		///<typeparam name="T">The element type of the collection.</typeparam>
+		///<param cref="elements">An ICollection of the specified type <seealso cref="T" />.</param>
+		///<exception cref="System.ArgumentOutOfRangeException">Thrown if the ICollection is empty.</exception>
+		///<exception cref="AMT.Extensions.Linq.AllElementsExcluded">Thrown if all elements have been excluded.</exception>
+		public static T Random<T> (this ICollection<T> elements, IEnumerable<T> exclusions = null)
+		{
+			// Delegate to overload taking IEnumerable 
+			return Random<T>(elements as IEnumerable<T>, exclusions);
+		}
+
+
+		///<summary>Returns a random element using an enumerable.</summary>
+		///<typeparam name="T">The element type of the enumerable.</typeparam>
+		///<param cref="elements">An IEnumerable of the specified type <seealso cref="T" />.</param>
+		///<exception cref="System.ArgumentOutOfRangeException">Thrown if the IEnumerable is empty.</exception>
+		///<exception cref="AMT.Extensions.Linq.AllElementsExcluded">Thrown if all elements have been excluded.</exception>
+		public static T Random<T>(this IEnumerable<T> elements, IEnumerable<T> exclusions = null)
+		{
+			exclusions ??= new List<T>();
+
+			T result;
+			int iterations = 0;
+			do
+			{
+				result = elements.ElementAt(_randomizer.Next(0, elements.Count() - 1));
+				++iterations;
+			} while (exclusions.Contains<T>(result) && iterations < elements.Count());
+
+			// TODO: this approach assumes that each element has been considered. If the randomizer
+			//	repeats some values, not all elements will be considered.
+			
+			// Reached the end of the collection (possibly) without finding an element that is not excluded.
+			if (iterations == elements.Count() && exclusions.Contains(result))
+			{
+				throw new AllElementsExcluded();
+			}
+
+			return result;
+		}
+
+
+		#endregion Public LINQ methods
+	}
+
+}
---
+++ Test.AMT.Extensions.Linq/IntListRandomizerTests.cs
@@ -0,0 +1,159 @@
+﻿using AMT.Extensions.Linq;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+
+namespace Test.AMT.Extensions.Linq
+{
+    // TODO: include when doing code coverage
+    // [ExcludeFromCodeCoverage]
+
+	public class IntListRandomizerTests : IClassFixture<IntListTestFixture>
+	{
+		IntListTestFixture _testData;
+
+
+		public IntListRandomizerTests(IntListTestFixture testData)
+		{
+			_testData = testData;
+		}
+
+
+		#region Positive tests
+
+		[Fact]
+		public void random_element_from_int_list()
+		{
+			IEnumerable<int> enr = _testData.TestList as IEnumerable<int>;
+
+			for (int i = 0; i < _testData.TestList.Count; ++i)
+			{
+				// Test via the method taking ICollection
+				int curr = _testData.TestList.Random();
+				curr.Should().BeGreaterOrEqualTo(IntListTestFixture.MinTestValue);
+
+				// Test via the method taking IEnumerable
+				curr = enr.Random();
+				curr.Should().BeGreaterOrEqualTo(IntListTestFixture.MinTestValue);
+			}
+		}
+
+
+		[Fact]
+		public void random_element_from_single_item_list()
+		{
+			// Create a list with only 1 element
+			int knownInt = 7466;
+			List<int> list = new List<int>();
+			list.Add(knownInt);
+			IEnumerable<int> enr = list as IEnumerable<int>;
+
+			// Get random from the list several times. Should only receive the single element.
+			for (int i = 0; i < 5; ++i)
+			{
+				// Test via the method taking ICollection
+				(list.Random()).Should().Be(knownInt);
+
+				// Test via the method taking IEnumerable
+				(enr.Random()).Should().Be(knownInt);
+			}
+		}
+
+
+		[Fact]
+		public void random_element_from_Dictionary()
+		{
+			// Create a collection with asso'd enumerator, and populate it.
+			Dictionary<int, string> dict = new Dictionary<int, string>();
+			IEnumerable<KeyValuePair<int, string>> enr = dict as IEnumerable<KeyValuePair<int, string>>;
+			for (int i = 0; i < 250; ++i) { dict.Add(i, i.ToString()); }
+
+
+			for (int i = 0; i < dict.Count; ++i)
+			{
+				// Test via the method taking ICollection
+				var curr = dict.Random();
+				curr.Key.Should().BeGreaterOrEqualTo(IntListTestFixture.MinTestValue);
+				curr.Value.Should().BeEquivalentTo(curr.Key.ToString());
+
+				// Test via the method taking IEnumerable
+				curr = enr.Random();
+				curr.Key.Should().BeGreaterOrEqualTo(IntListTestFixture.MinTestValue);
+				curr.Value.Should().BeEquivalentTo(curr.Key.ToString());
+			}
+		}
+
+
+		[Fact]
+		public void random_element_from_SortedList()
+		{
+			// Create a collection with asso'd enumerator, and populate it.
+			SortedList<int, string> list = new SortedList<int, string>();
+			IEnumerable<KeyValuePair<int, string>> enr = list as IEnumerable<KeyValuePair<int, string>>;
+			for (int i = 0; i < 250; ++i) { list.Add(i, i.ToString()); }
+
+			for (int i = 0; i < list.Count; ++i)
+			{
+				// Test via the method taking ICollection
+				var curr = list.Random();
+				curr.Key.Should().BeGreaterOrEqualTo(IntListTestFixture.MinTestValue);
+				curr.Value.Should().BeEquivalentTo(curr.Key.ToString());
+
+				// Test via the method taking IEnumerable
+				curr = enr.Random();
+				curr.Key.Should().BeGreaterOrEqualTo(IntListTestFixture.MinTestValue);
+				curr.Value.Should().BeEquivalentTo(curr.Key.ToString());
+			}
+		}
+
+		#endregion Positive tests
+		
+
+
+		#region Negative tests
+
+		// Verify the correct exception occurs when passed an empty list
+		[Fact]
+		public void verify_excp_on_empty_list()
+		{
+			List<string> list = new List<string>();
+
+			// Test via the method taking ICollection
+			Action act = () => list.Random();
+			act.Should().Throw<ArgumentOutOfRangeException>();
+
+			// Test via the method taking IEnumerable
+			act = () => (list as IEnumerable<string>).Random();
+			act.Should().Throw<ArgumentOutOfRangeException>();
+		}
+
+
+		[Fact]
+		public void verify_excp_after_all_excluded()
+		{
+			// Build a list and copy it to exclusions
+			List<string> list = new List<string>();
+			list.Add("one");
+			list.Add("two");
+			string[] exclusions = new string[list.Count];
+			list.CopyTo(exclusions);
+
+			// Since all the list values will be excluded, the
+			//	exception will occur.
+
+			// Test via the method taking ICollection
+			Action act = () => list.Random(exclusions);
+			act.Should().Throw<AllElementsExcluded>();
+
+			// Test via the method taking IEnumerable
+			act = () => (list as IEnumerable<string>).Random(exclusions);
+			act.Should().Throw<AllElementsExcluded>();
+		}
+
+		#endregion Negative tests
+		
+	}
+
+}
